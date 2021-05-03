8f5b7471fb61f2251813769d4ec6ec4a214ab4bf
chiboycalix <igwechinonso77@gmail.com>
2021-05-03 19:03:28 +0100

    add animation to app features section

--- src/sections/Features/index.js
+++ src/sections/Features/index.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './styles.scss';
 import GooglePlay from '../../assets/img/google-play.svg';
 import FeaturesPhone from '../../assets/img/features-phone-one.png';
 import { AiFillApple } from 'react-icons/all';
+import { useIsVisible } from "react-is-visible";
+
 
 const Features = () => {
+  const leftContentRef = React.useRef();
+  const rightContentRef = React.useRef();
+  const leftContentVisible = useIsVisible(leftContentRef);
+  const rightContentVisible = useIsVisible(rightContentRef);
+
   return (
     <div className="featuresWrapper">
       <div className="featuresHeader">
@@ -11,7 +19,8 @@ const Features = () => {
         <p>Schooln.ng brings all of your different social media apps together on a single platform. Providing you with the best and more focused way of performing your regular social media activities.</p>
       </div>
       <div className="featuresContent">
-        <div className="featuresContentLeft">
+
+        <div ref={leftContentRef} className={`featuresContentLeft ${leftContentVisible ? 'scrollInLeft' : ''}`}>
           <div className="featuresCard">
             <span>1</span>
             <p>Schooln.ng allows its user share information/gist from their school and personal life with others on the platform.</p>
@@ -20,12 +29,12 @@ const Features = () => {
             <span>3</span>
             <p>Schooln.ng connects you with peers within same institution as well as across other institutions on the app by sending peer request, like, share and comment on gist, flag down false information.</p>
           </div>
-
         </div>
+
         <div className="featuresContentCenter">
           <img src={FeaturesPhone} alt="FeaturesPhone" />
         </div>
-        <div className="featuresContentRight">
+        <div ref={rightContentRef} className={`featuresContentRight ${rightContentVisible ? 'scrollInRight' : ''}`}>
           <div className="featuresCard">
             <span>2</span>
             <p>Schooln.ng empower students to build and grow businesses around and beyond their schools while growing academically through peer-to-learning.</p>
@@ -55,7 +64,7 @@ const Features = () => {
           </span>
         </a>
       </div>
-    </div>
+    </div >
   )
 }
 
